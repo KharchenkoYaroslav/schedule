e36e95135c00e73adc86ab04d3e5700780917205
Yaroslav <kharchenko.yaroslav@lll.kpi.ua>
2024-11-16 11:32:42 +0200

    modified:   src/components/getData.ts

--- src/components/getData.ts
+++ src/components/getData.ts
@@ -34,7 +34,7 @@ const cur_semester = () => {
 
 async function FetchScheduleForGroup(groupName: string): Promise<GroupSchedule | null> {
     try {
-        const response = await axios.get(`https://schedule-server-rho.vercel.app/api/getGroup?getGroup?groupName=${groupName}&semester=${cur_semester()}`);
+        const response = await axios.get(`https://schedule-server-rho.vercel.app/api/getGroup?groupName=${groupName}&semester=${cur_semester()}`);
 
         if (!response.data || response.data.length === 0) {
             return null;
