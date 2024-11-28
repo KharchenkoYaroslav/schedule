2834247fefbbe316c2e2a0aca391789b8a27967e
Yaroslav <kharchenko.yaroslav@lll.kpi.ua>
2024-11-28 09:15:28 +0200

    try 	modified:   src/components/useWindowResize.ts

--- src/components/useWindowResize.ts
+++ src/components/useWindowResize.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 const useWindowResize = () => {
     const [scale, setScale] = useState<number>(1);
+    const [isMobile, setIsMobile] = useState<boolean>(false);
 
     useEffect(() => {
         const handleResize = () => {
@@ -14,23 +15,45 @@ const useWindowResize = () => {
             }
         };
 
-        const mediaQuery = window.matchMedia('(max-width: 899px)');
-
-        const handleMediaQueryChange = (event: MediaQueryListEvent) => {
-            if (event.matches) {
-                handleResize();
-            } else {
-                setScale(1);
-            }
+        const handleOrientationChange = () => {
+            handleResize();
         };
 
-        mediaQuery.addEventListener('change', handleMediaQueryChange);
-        handleResize(); // Виклик при монтуванні компонента
-
-        return () => {
-            mediaQuery.removeEventListener('change', handleMediaQueryChange);
+        // Визначаємо, чи є пристрій мобільним
+        const checkIsMobile = () => {
+            const mediaQuery = window.matchMedia('(max-width: 768px)');
+            setIsMobile(mediaQuery.matches);
         };
-    }, []);
+
+        checkIsMobile();
+
+        if (isMobile) {
+            window.addEventListener('resize', handleResize);
+            window.addEventListener('orientationchange', handleOrientationChange);
+        } else {
+            const mediaQuery = window.matchMedia('(max-width: 899px)');
+            const handleMediaQueryChange = (event: MediaQueryListEvent) => {
+                if (event.matches) {
+                    handleResize();
+                } else {
+                    setScale(1);
+                }
+            };
+            mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+            // Виклик при монтуванні компонента
+            handleResize();
+
+            return () => {
+                if (isMobile) {
+                    window.removeEventListener('resize', handleResize);
+                    window.removeEventListener('orientationchange', handleOrientationChange);
+                } else {
+                    mediaQuery.removeEventListener('change', handleMediaQueryChange);
+                }
+            };
+        }
+    }, [isMobile]);
     
     return scale;
 };
